2c7a17b97f90ebf63546bc519f2800c8198d32f0
Diego Huaraca <diego.21.phs@hotmail.com>
2015-05-07 16:06:53 -0500

    Script Curso

---
+++ Script.R
@@ -0,0 +1,14 @@
+## Curso R Nivel Basico ##
+## Source Stat Lab Ec ##
+
+## Instalación de librerías
+
+
+
+## Actualización de librerias
+
+
+## Instalación de R & RStudio
+
+
+## Eliminacion de librerias.
