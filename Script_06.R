966aedaddb98107edbc08147584d33e6b2e63ba0
Diego Huaraca <diego.21.phs@hotmail.com>
2015-06-02 23:21:53 -0500

    Script Funciones

--- Script_06.R
+++ Script_06.R
@@ -11,3 +11,47 @@ casino <- function(){
 ## Activamos el juego
 casino()
 
+
+## Sentencias IF
+# Ejercicio 1
+num <- -2
+if (num<0){ 
+      num<-num*-1
+}
+
+# Ejercicio 2
+num <- 4
+if (num<0){ 
+      num<-num*-1
+}
+
+# Ejercicio 3
+num <- -2
+if (num<0){
+      print("El número ingresado es negativo.") 
+      print("No hay que preocuparse. Ya lo reparamos.") 
+      num <- num*-1
+      print("Ahora el número es positivo.")
+}
+
+# Ejercicio 4
+# Que valor de x retorna ?
+x <- 1 
+if (x==1){
+      x <- 2 
+      if (x==1){
+            x <- 3
+      }
+}
+
+
+## Sentencias ELSE
+
+
+## Sentencias FOR
+
+
+## Sentencia WHILE
+
+
+## Sentencia BREAK
