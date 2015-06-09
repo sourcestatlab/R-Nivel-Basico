dcec30b66f2ca16f72be38f187288b0f0c2d6039
Diego Huaraca <diego.21.phs@hotmail.com>
2015-06-09 15:20:40 -0500

    Script 06

--- Script_06.R
+++ Script_06.R
@@ -18,21 +18,24 @@ num <- -2
 if (num<0){ 
       num<-num*-1
 }
+num
 
 # Ejercicio 2
 num <- 4
 if (num<0){ 
       num<-num*-1
 }
+num
 
 # Ejercicio 3
 num <- -2
 if (num<0){
       print("El número ingresado es negativo.") 
-      print("No hay que preocuparse. Ya lo reparamos.") 
+      print("No hay de que preocuparse. Ya lo reparamos.") 
       num <- num*-1
       print("Ahora el número es positivo.")
 }
+num
 
 # Ejercicio 4
 # Que valor de x retorna ?
@@ -46,9 +49,47 @@ if (x==1){
 
 
 ## Sentencias ELSE
+# Ejercicio 5
+x <- 4 # Ingrese un valor
+if(x>=0){
+      print("Número Positivo")
+} else {
+      print("Número negativo")
+}
+
+
+# Ejercicio 6 (Valor absoluto)
+x <- -12 # Ingrese un valor
+if(x>=0){
+      res <- sqrt(x)
+} else {
+      res <- sqrt(-x)
+}
+res
+
+
+# Ejercicio 7
+a <- rnorm(1, mean=1, sd=3)
+b <- rnorm(1, mean=1, sd=3)
+if(a>b){ 
+      print("Ganó A!")
+} else if(a<b){ 
+      print("Ganó B!")
+} else { 
+      print("Empate.")
+}
 
 
 ## Sentencias FOR
+# Ejercicio 8
+for(i in 1:10) {
+      print(i)
+}
+
+
+# Ejercicio 9
+
+
 
 
 ## Sentencia WHILE
